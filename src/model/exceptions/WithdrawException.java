0dbff5c1b68d2957148de67f479396689b32907c
willieny <wbm@ic.ufal.br>
2020-06-28 19:58:31 -0300

    Projeto com tratamento de exceção

---
+++ src/model/exceptions/WithdrawException.java
@@ -0,0 +1,11 @@
+package model.exceptions;
+
+public class WithdrawException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public WithdrawException(String msg) {
+		super(msg);
+	}
+
+}
